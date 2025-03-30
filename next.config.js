71f672c14d75d38b6b349ad345b64188f2c59862
Pankaj Ahuja <pankajahuja2212@gamil.com>
2025-03-30 21:34:59 +0530

    images path updation

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   distDir: "out",    // Output directory (default for Next.js export)
   images: { unoptimized: true }, // Required if using Next.js Image component
   // basePath: "", // Replace with your GitHub repo name
-  assetPrefix: "./", // Replace with your GitHub repo name
+  assetPrefix: "/", // Replace with your GitHub repo name
 };
 
 module.exports = nextConfig;
