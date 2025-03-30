20fe9944bebf2e30198fb0e590bb79bd3353f391
Pankaj Ahuja <pankajahuja2212@gamil.com>
2025-03-30 21:02:22 +0530

    images path updation

--- utils/data/codingProfiles.js
+++ utils/data/codingProfiles.js
@@ -1,22 +1,22 @@
 export const codingProfiles = [
   {
     name: 'LeetCode',
-    icon: '/icons/leetcode.svg',
+    icon: 'icons/leetcode.svg',
     url: 'https://leetcode.com/u/Skull_crusher44/', // Ensure this is correct
   },
   {
     name: 'GeeksforGeeks',
-    icon: '/icons/geeksforgeek.svg', // Using LeetCode icon as a placeholder
+    icon: 'icons/geeksforgeek.svg', // Using LeetCode icon as a placeholder
     url: 'https://www.geeksforgeeks.org/user/pankajahuja988/', // Ensure this is correct
   },
   {
     name: 'CodeChef',
-    icon: '/icons/codechef.svg', // Using LeetCode icon as a placeholder
+    icon: 'icons/codechef.svg', // Using LeetCode icon as a placeholder
     url: 'https://www.codechef.com/users/skull_crusher4', // Ensure this is correct
   },
   {
     name: 'Codeforces',
-    icon: '/icons/codeforces.png', // Using LeetCode icon as a placeholder
+    icon: 'icons/codeforces.png', // Using LeetCode icon as a placeholder
     url: 'https://codeforces.com/profile/Skull6393', // Ensure this is correct
   },
 ];
