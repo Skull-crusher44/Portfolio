9b3ff3232f7bb7a7d567c3d88f10151fb06d2cf4
Pankaj Ahuja <pankajahuja2212@gamil.com>
2025-03-30 15:50:59 +0530

    update to deploy on vercel

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,9 @@
 import { GoogleTagManager } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/footer";
 import ScrollToTop from "./components/helper/scroll-to-top";
 import Navbar from "./components/navbar";
+import ToastWrapper from "./components/toast-wrapper";
 import "./css/card.scss";
 import "./css/globals.scss";
 const inter = Inter({ subsets: ["latin"] });
@@ -18,8 +17,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ToastContainer />
+      <body className={inter.className} suppressHydrationWarning>
+        <ToastWrapper />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
           {children}
