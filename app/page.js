d6d2a79f68357af4e6b7007a307c7eae9308bd2c
Pankaj Ahuja <pankajahuja2212@gamil.com>
2025-03-30 17:57:24 +0530

    deploy

--- app/page.js
+++ app/page.js
@@ -15,8 +15,8 @@ export default function Home() {
       <Projects />
       <Skills />
       <CodingProfiles />
-      {/* <Experience />
-      <Education /> */}
+      <Experience />
+      {/* <Education /> */}
       <ContactSection />
     </div>
   );
